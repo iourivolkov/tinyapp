6b9df092f73ff6380240acf2bfc4afc78e7d60d2
iourivolkov <volk.iouri@gmail.com>
2021-11-08 20:27:15 +0000

    add route to send response containing html

--- express_server.js
+++ express_server.js
@@ -14,7 +14,11 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
-
+// add route to json url database
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});
+// add route --> app.get(path, callback)
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
