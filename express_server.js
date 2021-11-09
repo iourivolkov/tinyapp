ca00963a0c22459861e2bf732ef78250f177880c
iourivolkov <volk.iouri@gmail.com>
2021-11-09 02:34:12 +0000

    add body parser for POST request

--- express_server.js
+++ express_server.js
@@ -10,6 +10,7 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -18,10 +19,18 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+
 // add route to json url database
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
+
+// makes POST request data readable
+// converts request body from a Buffer into a readable string
+// adds the data to the req object under the key - body
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 // add route --> app.get(path, callback)
 app.get("/hello", (req, res) => {
@@ -35,9 +44,21 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 })
 
+// add GET route to show the form / render the urls_new template
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 // add a second route and template
 // ":" indicates shortURL is a route parameter
+// /urls/:d route 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
+
+
+
+
+
+// add a POST route to handle the form submission 
